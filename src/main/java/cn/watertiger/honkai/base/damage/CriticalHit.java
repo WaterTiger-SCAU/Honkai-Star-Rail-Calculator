1ea87b58b0dd976243c22e5cabc1ac92edadc4a4
水虎 <1041827329@qq.com>
2023-08-09 23:53:58 +0800

    feat(critical): 暴击计算

---
+++ src/main/java/cn/watertiger/honkai/base/damage/CriticalHit.java
@@ -0,0 +1,41 @@
+package cn.watertiger.honkai.base.damage;
+
+public class CriticalHit {
+    /**
+     * 暴击率
+     */
+    private double criticalRate;
+    /**
+     * 暴击伤害
+     */
+    private double criticalDamage;
+
+    public CriticalHit(double criticalRate, double criticalDamage) {
+        this.criticalRate = criticalRate;
+        this.criticalDamage = criticalDamage;
+    }
+
+    public double getCriticalExpect() {
+        return criticalRate * criticalDamage;
+    }
+
+    /* Getter & Setter */
+
+    public double getCriticalRate() {
+        return criticalRate;
+    }
+
+    public CriticalHit setCriticalRate(double criticalRate) {
+        this.criticalRate = criticalRate;
+        return this;
+    }
+
+    public double getCriticalDamage() {
+        return criticalDamage;
+    }
+
+    public CriticalHit setCriticalDamage(double criticalDamage) {
+        this.criticalDamage = criticalDamage;
+        return this;
+    }
+}
