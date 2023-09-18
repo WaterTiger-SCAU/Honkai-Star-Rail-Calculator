4a28e1df2177f52c105ab6c3028bfaf367fe1df2
水虎 <1041827329@qq.com>
2023-09-18 23:34:35 +0800

    feat(util): 浮点数计算工具

---
+++ src/main/java/cn/watertiger/honkai/base/util/DoubleMathUtil.java
@@ -0,0 +1,78 @@
+package cn.watertiger.honkai.base.util;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * 双精度浮点数计算工具
+ * 默认保留两位小数
+ *
+ * @author water-tiger
+ */
+public class DoubleMathUtil {
+    private DoubleMathUtil() {
+    }
+
+    /**
+     * 不会丢失精度的加法计算
+     *
+     * @param a 加数
+     * @param b 被加数
+     * @return 结果
+     */
+    public static double add(double a, double b) {
+        BigDecimal b1 = BigDecimal.valueOf(a);
+        BigDecimal b2 = BigDecimal.valueOf(b);
+
+        return b1.add(b2)
+                .setScale(2, RoundingMode.HALF_UP)
+                .doubleValue();
+    }
+
+    /**
+     * 不会丢失精度的减法计算
+     *
+     * @param a 减数
+     * @param b 被减数
+     * @return 结果
+     */
+    public static double subtract(double a, double b) {
+        BigDecimal b1 = BigDecimal.valueOf(a);
+        BigDecimal b2 = BigDecimal.valueOf(b);
+
+        return b1.subtract(b2)
+                .setScale(2, RoundingMode.HALF_UP)
+                .doubleValue();
+    }
+
+    /**
+     * 不会丢失精度的乘法计算
+     *
+     * @param a 乘数
+     * @param b 被乘数
+     * @return 结果
+     */
+    public static double multiply(double a, double b) {
+        BigDecimal b1 = BigDecimal.valueOf(a);
+        BigDecimal b2 = BigDecimal.valueOf(b);
+
+        return b1.multiply(b2)
+                .setScale(2, RoundingMode.HALF_UP)
+                .doubleValue();
+    }
+
+    /**
+     * 不会丢失精度的除法计算
+     *
+     * @param a 除数
+     * @param b 被除数
+     * @return 结果
+     */
+    public static double divide(double a, double b) {
+        BigDecimal b1 = BigDecimal.valueOf(a);
+        BigDecimal b2 = BigDecimal.valueOf(b);
+
+        return b1.divide(b2, 2, RoundingMode.HALF_UP)
+                .doubleValue();
+    }
+}
---
+++ src/test/java/cn/watertiger/honkai/base/util/DoubleMathUtilTest.java
@@ -0,0 +1,82 @@
+package cn.watertiger.honkai.base.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.stream.Stream;
+
+class DoubleMathUtilTest {
+    public static Stream<Arguments> doubleCalculateProvider() {
+        return Stream.of(
+                Arguments.of(1.1, 1.3)
+        );
+    }
+
+    private boolean newScaleEqTwo(double d) {
+        return BigDecimal.valueOf(d).setScale(2, RoundingMode.HALF_UP).doubleValue() == d;
+    }
+
+    @ParameterizedTest
+    @MethodSource("doubleCalculateProvider")
+    void testAdd(double a, double b) {
+        double doubleOperationResult = a + b;
+        double utilOperationResult = DoubleMathUtil.add(a, b);
+
+        System.out.println("doubleOperationResult:" + doubleOperationResult);
+        System.out.println("utilOperationResult:" + utilOperationResult);
+
+        Assertions.assertTrue(
+                !newScaleEqTwo(doubleOperationResult)
+                && newScaleEqTwo(utilOperationResult)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("doubleCalculateProvider")
+    void testSubtract(double a, double b) {
+        double doubleOperationResult = a - b;
+        double utilOperationResult = DoubleMathUtil.subtract(a, b);
+
+        System.out.println("doubleOperationResult:" + doubleOperationResult);
+        System.out.println("utilOperationResult:" + utilOperationResult);
+
+        Assertions.assertTrue(
+                !newScaleEqTwo(doubleOperationResult)
+                        && newScaleEqTwo(utilOperationResult)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("doubleCalculateProvider")
+    void testMultiply(double a, double b) {
+        double doubleOperationResult = a * b;
+        double utilOperationResult = DoubleMathUtil.multiply(a, b);
+
+        System.out.println("doubleOperationResult:" + doubleOperationResult);
+        System.out.println("utilOperationResult:" + utilOperationResult);
+
+        Assertions.assertTrue(
+                !newScaleEqTwo(doubleOperationResult)
+                        && newScaleEqTwo(utilOperationResult)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("doubleCalculateProvider")
+    void testDivide(double a, double b) {
+        double doubleOperationResult = a / b;
+        double utilOperationResult = DoubleMathUtil.divide(a, b);
+
+        System.out.println("doubleOperationResult:" + doubleOperationResult);
+        System.out.println("utilOperationResult:" + utilOperationResult);
+
+        Assertions.assertTrue(
+                !newScaleEqTwo(doubleOperationResult)
+                        && newScaleEqTwo(utilOperationResult)
+        );
+    }
+}
